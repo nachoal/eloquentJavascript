76d06ca3d5c710dbb01e64e58f7f1692db115697
nachoal <nacho.allibre@gmail.com>
2020-04-09 18:42:48 -0500

    Create Vec class

--- chapter_6/chapter_6_1.js
+++ chapter_6/chapter_6_1.js
@@ -9,6 +9,26 @@
   the distance of the point (x, y) from the origin (0, 0).
 */
 
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(vector) {
+    return new Vec(this.x + vector.x, this.y + vector.y);
+  }
+
+  minus(vector) {
+    return new Vec(this.x - vector.x, this.y - vector.y);
+  }
+
+  get length() {
+    // To calculate distance from (0,0) to (x,y):Pythagorean theorem: √(x2 + y2)
+    return Math.sqrt(this.x ** 2 + this.y ** 2);
+  }
+}
+
 console.log(new Vec(1, 2).plus(new Vec(2, 3)));
 // → Vec{x: 3, y: 5}
 console.log(new Vec(1, 2).minus(new Vec(2, 3)));
