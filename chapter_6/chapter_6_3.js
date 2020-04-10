fc477b21507370bf4485bb26897ecdffa0565b20
nachoal <nacho.allibre@gmail.com>
2020-04-09 22:12:32 -0500

    wip

--- chapter_6/chapter_6_3.js
+++ chapter_6/chapter_6_3.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-classes-per-file */
 /*
 
   ITERABLE GROUPS
@@ -13,7 +14,64 @@
 
 */
 
-for (let value of Group.from(["a", "b", "c"])) {
+class GroupIterator {
+  constructor(group) {
+    this.x = 0;
+    this.group = group;
+  }
+
+  next() {
+    if (this.x === this.group[this.group.length - 1])
+      return { value: undefined, done: true };
+
+    let value = {
+      x: this.x,
+      value: this.group[x],
+    };
+    this.x += 1;
+    return { value, done: false };
+  }
+}
+class Group {
+  constructor() {
+    this.group = [];
+    this.x = 0;
+    this.y = 0;
+  }
+
+  add(value) {
+    if (this.group.indexOf(value) === -1) {
+      this.group.push(value);
+    }
+  }
+
+  delete(value) {
+    const index = this.group.indexOf(value);
+    if (index !== -1) {
+      this.group.splice(index, 1);
+    }
+  }
+
+  has(value) {
+    return this.group.indexOf(value) !== -1;
+  }
+
+  static from(iterableObject) {
+    const group = new Group();
+
+    iterableObject.forEach((element) => {
+      group.add(element);
+    });
+
+    return group;
+  }
+
+  [Symbol.iterator]() {
+    return new GroupIterator(this);
+  }
+}
+
+for (let value of Group.from(['a', 'b', 'c'])) {
   console.log(value);
 }
 // → a
--- chapter_6/chapter_6_4.js
+++ chapter_6/chapter_6_4.js
@@ -13,5 +13,5 @@
 let map = { one: true, two: true, hasOwnProperty: true };
 
 // Fix this call
-console.log(map.hasOwnProperty("one"));
+console.log(map.hasOwnProperty('one'));
 // → true
