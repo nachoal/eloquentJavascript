a5c79555fc6e9539a86cee5ad6451779e4626406
nachoal <nacho.allibre@gmail.com>
2020-04-13 15:48:29 -0500

    Add error catching function

---
+++ chapter_9/chapter_9_1.js
@@ -0,0 +1,35 @@
+/*
+  RETRY
+  Say you have a function primitiveMultiply that in 20 percent of cases multiplies
+  two numbers and in the other 80 percent of cases raises an exception of type
+  MultiplicatorUnitFailure.
+
+  Write a function that wraps this clunky function and just
+  keeps trying until a call succeeds, after which it returns the result.
+
+  Make sure you handle only the exceptions you are trying to handle.
+*/
+
+class MultiplicatorUnitFailure extends Error {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  }
+
+  throw new MultiplicatorUnitFailure('Klunk');
+}
+
+function reliableMultiply(a, b) {
+  try {
+    return primitiveMultiply(a, b);
+  } catch (error) {
+    if (error instanceof MultiplicatorUnitFailure) {
+      return reliableMultiply(a, b);
+    }
+    return error;
+  }
+}
+
+console.log(reliableMultiply(8, 8));
+// → 64
