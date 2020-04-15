d318823e81588a14caf955cb46d04fac8328a5fa
nachoal <nacho.allibre@gmail.com>
2020-04-14 22:16:28 -0500

    Build CommonJS module

---
+++ chapter_10/graph.js
@@ -0,0 +1,22 @@
+/*
+  ROADS MODULE
+*/
+
+exports.buildGraph = function (edges) {
+  const graph = Object.create(null);
+
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
+    }
+  }
+
+  for (const [from, to] of edges.map((r) => r.split('-'))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+
+  return graph;
+};
